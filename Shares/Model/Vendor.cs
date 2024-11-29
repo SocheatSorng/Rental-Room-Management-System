186304bba799ba29305bcac77907e75bd791bf68
Socheat <sorngsocheat@gmail.com>
2024-11-29 19:09:28 +0700

    Update Vendor.cs

--- Shares/Model/Vendor.cs
+++ Shares/Model/Vendor.cs
@@ -8,17 +8,17 @@ namespace RRMS.Model
 {
     public class Vendor
     {
-        public int VendorID { get; set; }
-        public string VendorName { get; set; }
-        public string VendorContact {  get; set; }
-        public string VendorHNo { get; set; }
-        public string VendorSNo { get; set; }
-        public string VendorCommune { get; set; }
-        public string VendorDistrict { get; set; }
-        public string VendorProvince { get; set; }
-        public DateTime VendorConStart { get; set; }
-        public DateTime VendorConEnd {  get; set; }
-        public Boolean VendorStatus { get; set; }
-        public string VendorDesc { get; set; }
+        public int VenID { get; set; }
+        public string VenName { get; set; }
+        public string VenContact {  get; set; }
+        public string VenHNo { get; set; }
+        public string VenSNo { get; set; }
+        public string VenCommune { get; set; }
+        public string VenDistrict { get; set; }
+        public string VenProvince { get; set; }
+        public DateTime VenConStart { get; set; }
+        public DateTime VenConEnd {  get; set; }
+        public Boolean VenStatus { get; set; }
+        public string VenDesc { get; set; }
     }
 }
